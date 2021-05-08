5c8cf553b207392fb627e2cae508fd4c1e585fcf
chuzhaozhi <>
2021-05-08 11:18:39 +0800

    first commit

---
+++ JC_Tools/JC_Tools.h
@@ -0,0 +1,24 @@
+//
+//  JC_Tools.h
+//  JC_Tools
+//  Created by JackerooChu on 2021/5/8.
+//  
+//
+//  
+//  If debugging a program bothers you, don't give up. 
+//	Success is always around the corner. 
+//	You never know how far you are from him unless you come to the corner. 
+//	So remember,
+//	persevere until you succeed.
+//
+        
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JC_Tools : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JC_Tools/JC_Tools.m
@@ -0,0 +1,20 @@
+//
+//  JC_Tools.m
+//  JC_Tools
+//  Created by JackerooChu on 2021/5/8.
+//  
+//
+//  
+//  If debugging a program bothers you, don't give up. 
+//	Success is always around the corner. 
+//	You never know how far you are from him unless you come to the corner. 
+//	So remember,
+//	persevere until you succeed.
+//
+        
+
+#import "JC_Tools.h"
+
+@implementation JC_Tools
+
+@end
